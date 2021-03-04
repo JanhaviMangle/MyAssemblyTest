84c0c9d53251562561e1fc7e4eb4df71c1a1c2ac
Janhavi Mangle <ajaymangle577@gmail.com>
2021-03-04 22:43:22 +0530

    Commiting JDBC code v1

---
+++ GCExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssemblyTest
+{
+    class GCExample
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("GC maximum generations:" + GC.MaxGeneration);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLibrary;
+ 
+
+
+namespace MyAssemblyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello for testing My Assembly- change charecter");
+            Console.ReadKey();
+            ChangeCharecter c1 = new ChangeCharecter();
+           
+          
+        }
+    }
+}
